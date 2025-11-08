7765785ffa653cb2ebc2e2222ff85f6eacff52e6
elfuhh <dreadedmexoxo@gmail.com>
2025-11-08 17:02:45 -0500

    Create bypass.user.js

---
+++ bypass.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Volcano & Work.ink Bypass
+// @namespace    http://tampermonkey.net/
+// @version      1.7.0.0
+// @description  Bypass Volcano and Work.ink links
+// @author       elfuhh and imdyrian
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://cdn.discordapp.com/avatars/907504823325294642/9701f48d983a8f728695f3b5b7b12b91.png?size=1024
+// @require      https://github.com/ricogiantz/bypass/raw/main/bypass.js
+// ==/UserScript==
