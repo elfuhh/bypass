4ddf303eb68457467d8a38ba3c1bf7320a97a42c
elfuhh <dreadedmexoxo@gmail.com>
2025-11-08 19:05:47 -0500

    Update bypass.user.js

--- bypass.user.js
+++ bypass.user.js
@@ -8,6 +8,8 @@
 // @match        https://work.ink/*
 // @run-at       document-start
 // @grant        unsafeWindow
-// @icon         https://cdn.discordapp.com/avatars/907504823325294642/9701f48d983a8f728695f3b5b7b12b91.png?size=1024
+// @icon         https://cdn.discordapp.com/icons/1312680907559276576/bedc7397e04d863a9ee5c6087ece8ae8.png?size=80&quality=lossless
+// @downloadURL  https://github.com/elfuhh/bypass/raw/main/bypass.user.js
+// @updateURL    https://github.com/elfuhh/bypass/raw/main/bypass.user.js
 // @require      https://github.com/elfuhh/bypass/raw/main/bypass.js
 // ==/UserScript==
