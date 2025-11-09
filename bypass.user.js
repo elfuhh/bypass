0536bd7d62e08ee9ef6f10becf8491b2ddd2eb01
elfuhh <dreadedmexoxo@gmail.com>
2025-11-09 14:11:24 -0500

    Update bypass.user.js

--- bypass.user.js
+++ bypass.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Volcano & Work.ink Bypass
 // @namespace    http://tampermonkey.net/
-// @version      1.7.0.0
-// @description  Bypass Volcano and Work.ink links
-// @author       elfuhh and imdyrian
+// @version      1.8.0.0
+// @description  bypass Workinks and volcano's key system easily
+// @author       merged
 // @match        https://key.volcano.wtf/*
 // @match        https://work.ink/*
 // @run-at       document-start
 // @grant        unsafeWindow
-// @icon         https://cdn.discordapp.com/icons/1312680907559276576/bedc7397e04d863a9ee5c6087ece8ae8.png?size=80&quality=lossless
+// @icon         https://cdn.discordapp.com/avatars/907504823325294642/9701f48d983a8f728695f3b5b7b12b91.png?size=1024
 // @downloadURL  https://github.com/elfuhh/bypass/raw/main/bypass.user.js
 // @updateURL    https://github.com/elfuhh/bypass/raw/main/bypass.user.js
 // @require      https://github.com/elfuhh/bypass/raw/main/bypass.js
