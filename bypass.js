cc5c3d31549dcdb95da110f52a1d2f65f88ffd4a
elfuhh <dreadedmexoxo@gmail.com>
2025-11-08 19:55:14 -0500

    Update bypass.js

--- bypass.js
+++ bypass.js
@@ -1015,7 +1015,6 @@
                 let waitTimeSeconds = 5;
                 const url = location.href;
                 if (url.includes('42rk6hcq') || url.includes('ito4wckq') || url.includes('pzarvhq1')) {
-                    waitTimeSeconds = 38;
                 }
 
                 if (secondsPassed >= waitTimeSeconds) {
