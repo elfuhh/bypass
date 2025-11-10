8c0f0c29932a1337fdc51b034c6cfb2bc088f15f
elfuhh <dreadedmexoxo@gmail.com>
2025-11-09 19:54:59 -0500

    Update bypass.user.js

--- bypass.user.js
+++ bypass.user.js
@@ -12,5 +12,5 @@
 // @icon         https://cdn.discordapp.com/icons/1312680907559276576/bedc7397e04d863a9ee5c6087ece8ae8.png?size=80&quality=lossless
 // @downloadURL  https://github.com/elfuhh/bypass/raw/main/bypass.user.js
 // @updateURL    https://github.com/elfuhh/bypass/raw/main/bypass.user.js
-// @require      https://github.com/elfuhh/bypass/raw/main/bypass.js
+// @require https://cdn.jsdelivr.net/gh/elfuhh/bypass/bypass.js
 // ==/UserScript==
