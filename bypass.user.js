f710532c29a4ec1011cae9504537051b4d4a26a0
elfuhh <dreadedmexoxo@gmail.com>
2025-11-11 13:29:57 -0500

    Update bypass.user.js

--- bypass.user.js
+++ bypass.user.js
@@ -6,7 +6,6 @@
 // @author       Dyrian and elfuhh
 // @match        https://key.volcano.wtf/*
 // @match        https://work.ink/*
-// @match        https://volcano.wtf/instruction
 // @run-at       document-start
 // @grant        unsafeWindow
 // @icon         https://cdn.discordapp.com/icons/1312680907559276576/bedc7397e04d863a9ee5c6087ece8ae8.png?size=80&quality=lossless
